ef4b50c406a111e942b2a8a59acfc84b2b4a915f
mateoh2467 <144353650+mateoh2467@users.noreply.github.com>
2023-12-19 07:44:07 +0000

    did this homework

---
+++ gradebook.py
@@ -0,0 +1,41 @@
+#initial gradebook list for computer science class 
+gradebook = [100, "Variables Test", 75, "Variables Homework"]
+
+#function to print grades
+def print_grades():
+    print("Current gradebook: " + str(gradebook))
+
+#function to amend the gradebook
+def amend_gradebook(operation, grade_input, assignment_input):
+    if(operation == "add"):
+        gradebook.append(assignment_input)
+        gradebook.append(int(grade_input))
+    elif(operation == "remove"):
+        if(assignment_input in gradebook and int(grade_input) in gradebook):
+            gradebook.remove(assignment_input)
+            gradebook.remove(int(grade_input))
+        else:
+            print("Assignment or grade not found in gradebook")
+    else:
+        print_grades()
+#main function
+def main():
+    print("Welcome to John's CS Gradebook. ")
+    while True: 
+        user_input = input("What would you like to do? [Add] or [Remove] Grade or [Print] of type [exit] to quit: ").lower()
+        if(user_input == "add"):
+            assignment_input = input("Please enter the assignment name: ")
+            grade_input = input("Please enter grade: ")
+            amend_gradebook("add", grade_input, assignment_input)
+        elif(user_input == "remove"):
+            assignment_input = input("Please enter the assignment name you want to remove: ")
+            grade_input = input("Please enter the grade you want to remove: ")
+            amend_gradebook("remove", grade_input, assignment_input)
+        elif(user_input == "print"):
+            print_grades()
+        elif(user_input == "exit"):
+            print("Exiting Gradebook")
+            break
+
+#running the main function
+main()
